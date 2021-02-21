09ac477e92054b31892935104d50f71b0c8238af
akash prasher <prince.akash.prasher@gmail.com>
2021-02-21 16:47:31 +0530

    Fix minor issue
    Signed-off-by: akash prasher <prince.akash.prasher@gmail.com>

--- script/main.js
+++ script/main.js
@@ -2,9 +2,9 @@ $("#slideshow > div:gt(0)").hide();
 
 setInterval(function () {
   $("#slideshow > div:first")
-    .fadeOut(200)
+    .fadeOut(500)
     .next()
-    .fadeIn(200)
+    .fadeIn(500)
     .end()
     .appendTo("#slideshow");
 }, 3000);
